d58378827a72795b0d4e799aa8a0221d70749d37
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-01-02 17:31:34 -0300

    feat: new version

--- diamond/diamond.rb
+++ diamond/diamond.rb
@@ -1,33 +1,49 @@
 # frozen_string_literal: true
 
-# Write your code for the 'Diamond' exercise in this file. Make the tests in
-# `diamond_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/diamond` directory.
+# Diamond class responsible for creating a diamond pattern based on a given letter.
 class Diamond
+  attr_accessor :letter
+
+  # Generates a diamond pattern for a given letter.
   def self.make_diamond(letter)
+    # Return a single 'A' if the input is 'A'.
     return "A\n" if letter == 'A'
 
-    diamond(letter.upcase).join
+    # Instantiate a new Diamond object and generate the diamond pattern.
+    new.diamond(letter.upcase).join
   end
 
-  def self.max_spaces(letter)
-    letter.ord - 'A'.ord
-  end
-
-  def self.diamond(letter)
+  # Generates the lines for the diamond pattern based on the given letter.
+  def diamond(letter)
+    # Create an array of letters from 'A' to the given letter and back to 'A' excluding it.
     letters = ('A'..letter).to_a + ('A'...letter).to_a.reverse
 
+    # Generate each line of the diamond based on the letters array.
     letters.map do |char|
-      idx = char.ord - 'A'.ord
-      spaces = ' ' * (max_spaces(letter) - idx)
+      spaces = ' ' * max_spaces(letter, char)
 
       if char == 'A'
         "#{spaces}#{char}#{spaces}\n"
       else
-        "#{spaces}#{char}#{' ' * (2 * idx - 1)}#{char}#{spaces}\n"
+        "#{spaces}#{char}#{' ' * (2 * calc_char(char) - 1)}#{char}#{spaces}\n"
       end
     end
+  end
+
+  private
+
+  # Calculates the maximum spaces needed for each line based on the given letter and current character.
+  def max_spaces(letter, char)
+    calc_letter(letter) - calc_char(char)
+  end
+
+  # Calculates the distance of a given letter from 'A'.
+  def calc_letter(letter)
+    (letter.ord - 'A'.ord)
+  end
+
+  # Calculates the distance of a given character from 'A'.
+  def calc_char(char)
+    (char.ord - 'A'.ord)
   end
 end
